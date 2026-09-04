1fd3e41b28771b3c1226af2982555cf45d62e3b3
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2026-09-04 17:42:46 +0530

    added RestApi's i.e. Services and Controllers for Creating, Reading and Updating and Deleting the Journal Entries
    added JournalUpdateDTO for efficient updates in JournalEntry
    
    added spring-boot-stater-validation dependency for enforcing constraints in entities and DTOs.
    
    added Validation in JournalUpdateDTO.
    
    added size validation for DTO fields.

--- src/main/java/me/prabh/journal/controllers/JournalEntryController.java
+++ src/main/java/me/prabh/journal/controllers/JournalEntryController.java
@@ -1,9 +1,12 @@
 package me.prabh.journal.controllers;
 
+import me.prabh.journal.DTO.JournalUpdateDTO;
 import me.prabh.journal.entity.JournalEntry;
 import me.prabh.journal.service.JournalEntryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/journal")
@@ -13,13 +16,47 @@ public class JournalEntryController {
     @Autowired
     JournalEntryService journalEntryService;
 
+    //save entry
     @PostMapping
-    public JournalEntry postEntry(@RequestBody JournalEntry entry){
-        try {
-            return journalEntryService.saveEntry(entry);
-        }catch(Exception e) {
-            return null;
-        }
+    public JournalEntry postEntry(@RequestBody JournalEntry entry) {
+        return journalEntryService.saveEntry(entry);
     }
+
+    //get all entries
+    @GetMapping
+    public List<JournalEntry> getAllEntries() {
+        return journalEntryService.getAllEntries();
+    }
+
+    @GetMapping("/{id}")
+    public JournalEntry getEntryById(@PathVariable String id) {
+        return journalEntryService.getEntryById(id).get();
+    }
+
+    @GetMapping("/exists/{id}")
+    public boolean entryExists(@PathVariable String id) {
+        return journalEntryService.entryExists(id);
+    }
+
+    @GetMapping("/count")
+    public long countEntries(){
+        return journalEntryService.countEntries();
+    }
+
+    @DeleteMapping
+    public boolean deleteAllEntries(){
+        return journalEntryService.deleteAllEntries();
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteEntryById(@PathVariable String id) {
+        return journalEntryService.deleteAllEntries();
+    }
+
+    @PatchMapping("/{id}")
+    public JournalEntry udpateEntry(@PathVariable String id, @RequestBody JournalUpdateDTO dto) {
+        return journalEntryService.editEntry(id, dto.getTitle(), dto.getContent());
+    }
+
 
 }
--- src/main/java/me/prabh/journal/service/JournalEntryService.java
+++ src/main/java/me/prabh/journal/service/JournalEntryService.java
@@ -1,8 +1,10 @@
 package me.prabh.journal.service;
 import me.prabh.journal.entity.JournalEntry;
 import me.prabh.journal.repository.JournalEntryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JournalEntryService {
@@ -13,6 +15,7 @@ public class JournalEntryService {
         this.journalEntryRepository = journalEntryRepository;
     }
 
+    //save Entry
     public JournalEntry saveEntry(JournalEntry entry){
         try{
             return journalEntryRepository.save(entry);
@@ -20,5 +23,82 @@ public class JournalEntryService {
             throw new RuntimeException(e);
         }
     }
+
+    //get all entries
+    public List<JournalEntry> getAllEntries(){
+        try{
+            return journalEntryRepository.findAll();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //get entries by id
+    public Optional<JournalEntry> getEntryById(String id){
+        try{
+            return journalEntryRepository.findById(id);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //check if the journal exists.
+    public boolean entryExists(String id){
+        try{
+            return journalEntryRepository.existsById(id);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //count the number of entries
+    public long countEntries(){
+        try{
+            return journalEntryRepository.count();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //delete all entries
+    public boolean deleteAllEntries() {
+        try {
+            journalEntryRepository.deleteAll();
+            return true;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //delete entries by id
+    public boolean deleteEntryById(String id) {
+        try {
+            journalEntryRepository.deleteById(id);
+            return true;
+        }catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //edit title
+    public JournalEntry editEntry(String id, String newTitle, String newContent) {
+        try{
+            //find by id
+            Optional<JournalEntry> entryToBeUpdated = journalEntryRepository.findById(id);
+            entryToBeUpdated.ifPresent(entry -> {
+                //update the title.
+                entry.setTitle(newTitle);
+                entry.setContent(newContent);
+                journalEntryRepository.save(entry);
+            });
+            return entryToBeUpdated.get();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    //pagination - findAll - sorted.
+    //pagination - findAll - pageable.
 
 }
