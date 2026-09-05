03ecd7875a2f8d132f99d24c70d2f90bae89bd3a
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2026-09-05 17:59:45 +0530

    added lombok for automatic code generation at runtime for getters and setter and other fields.
    used in JournalEntry.java to generate getter and setters.

--- src/main/java/me/prabh/journal/entity/JournalEntry.java
+++ src/main/java/me/prabh/journal/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package me.prabh.journal.entity;
 
+import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 
 //POJO class for JournalEntry
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
 
     @Id
@@ -15,41 +17,9 @@ public class JournalEntry {
     private String title;
     private String content;
 
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
     @CreatedDate
     private LocalDateTime createdAt;
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
 }
